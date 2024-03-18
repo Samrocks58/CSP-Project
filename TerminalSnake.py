212c2b67fe7c9e59f1227d51323895c96b24270f
Samrocks58 <60971882+Samrocks58@users.noreply.github.com>
2024-03-18 11:38:01 -0400

    shifting the game to snake instead of tetris

--- TerminalSnake.py
+++ TerminalSnake.py
@@ -1,34 +1,41 @@
-import msvcrt, threading, os, time
+import msvcrt, os, time, random
 
 ROWS = 15+1
 MoveX = 8
 MoveY = 0
-blocks = [(MoveX, MoveY), (18, 10)]          
+passive_blocks = []
+snake_head = [MoveX, MoveY]
+snake_tail = [[MoveX-1, MoveY], [MoveX-2, MoveY], [MoveX-3, MoveY], [MoveX-4, MoveY]]
+coinPos = [random.randint(0,9)*2, random.randint(0, ROWS-2)]
+direction=1 #1: right 2: up 3: left 4: down
+blocks = snake_tail.extend(snake_head)
 def print_board():
-    global MoveX, MoveY, blocks
+    global MoveX, MoveY, active_blocks
     # while True:
     os.system('cls')
     # print("\033[H\033[J", end="")
     rowString = ""
     for i in range(ROWS):
         if i < ROWS-1:
-            # for b in blocks:
-            if i == MoveY:
-                rowString += "||" + " " * MoveX + "[]" + "\n"
-            else:
-                rowString += "|| \n"
+            rowString += "||"
+            spacesMoved = 0
+            for b in blocks:
+                if i == b[1]:
+                    rowString += " " * (b[0]-spacesMoved) + "[]"
+                    spacesMoved += b[0]+2
+            rowString += "\n"
         else:
             rowString += "  " + "-" * 20 + "\n"
     print(rowString)
     # print(f"MoveY: {MoveY}")
-    time.sleep(1/120)
+    time.sleep(1/16)
 
 
 # t2 = threading.Thread(target = print_board)
 # t2.start()
 
 def check_input():
-    global MoveX, MoveY, blocks
+    global MoveX, MoveY, active_blocks
     # while True:
     if msvcrt.kbhit():
         c = msvcrt.getwch()
@@ -42,22 +49,24 @@ def check_input():
             # print("LEFT")
             MoveX -= 2
             MoveX = max(0, MoveX)
-            blocks = [(MoveX, MoveY), (18, 10)]
+            for i in blocks:
+                i[0] -= 2
+                i[0] = max(0, i[0])
         elif c == 'M' or c == "d":
             # print("RIGHT")
-            MoveX += 2
-            MoveX = min(MoveX, 18)
-            blocks = [(MoveX, MoveY), (18, 10)]
+            for i in blocks:
+                i[0] += 2
+                i[0] = min(i[0], 18)
         elif c == 'H' or c == "w":
             # print("UP")
-            MoveY -= 1
-            MoveY = max(MoveY, 0)
-            blocks = [(MoveX, MoveY), (18, 10)]
+            for i in blocks:
+                i[1] -= 1
+                i[1] = max(i[1], 0)
         elif c == 'P' or c == "s":
             # print("DOWN")
-            MoveY += 1
-            MoveY = min(MoveY, ROWS-2)
-            blocks = [(MoveX, MoveY), (18, 10)]
+            for i in blocks:
+                i[1] += 1
+                i[1] = min(i[1], ROWS-2)
         # elif c != "à":
         #     print(f"\"{c}\"")
 
