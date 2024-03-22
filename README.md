a4ce49ac2069d6a841a086c05b4aecd70e8f5a25
Samrocks58 <60971882+Samrocks58@users.noreply.github.com>
2024-03-22 13:38:05 -0400

    got the snake to follow itself

---
+++ README.md
@@ -0,0 +1,27 @@
+# Terminal Snake
+
+This python-based program works to emulate the famous game Snake within the terminal using Windows system packages like msvcrt and os to get real-time user input within the terminal and clear the terminal before printing to make it seem like frames in a video game.
+
+## Background
+The main reason I made this program is because I thought it would be an interesting challenge to make an entire functioning game within the terminal. Also, I had heard a story of how the original creator of Tetris used very basic graphics within the terminal to create the original game and thought that was interesting. ou can control the snake with arrow keys or WASD, however, WASD usually has less input delay.
+
+## Download
+This code is available at this [repository on github](https://github.com/Samrocks58/CSP-Project)
+
+## Requirements
+* Python 3
+* Windows Operating System
+* Code Editor that supports Python
+
+## Execution
+* Download the code from the repository
+* Open up any editor that supports Python
+* Copy in the code and run it
+
+## Troubleshooting
+* If your inputs aren't working or are delayed, try using WASD instead of arrow keys
+* If no inputs are working at all, check that your system uses Windows Operating System
+* Check that you have Python set up correctly in your editor and that your version of Python has the libraries msvcrt, os, time, and random
+
+## Bibliography
+All libraries used are included with Python versions 3 and up.
--- TerminalSnake.py
+++ TerminalSnake.py
@@ -5,20 +5,16 @@ MoveX = 12
 MoveY = 4
 passive_blocks = []
 snake_head = [MoveX, MoveY]
-snake_tail = [[MoveX-1, MoveY], [MoveX-2, MoveY], [MoveX-3, MoveY], [MoveX-4, MoveY]]
 coinPos = [random.randint(0,9)*2, random.randint(0, ROWS-2)]
 direction=1 #1: right 2: up 3: left 4: down
-blocks=[]
-blocks.append(snake_head)
+blocks = [[MoveX-4, MoveY], [MoveX-3, MoveY], [MoveX-2, MoveY], [MoveX-1, MoveY], [MoveX, MoveY]]
 old_pos = [MoveX, MoveY]
-length = 5
-blocks.extend(snake_tail)
+length = 10
+keyPressed = False
 
 def print_board():
-    global MoveX, MoveY, blocks
-    # while True:
+    global blocks
     os.system('cls')
-    # print("\033[H\033[J", end="")
     rowString = ""
     for i in range(ROWS):
         if i == 0:
@@ -26,52 +22,59 @@ def print_board():
         if i < ROWS-1:
             rowString += "|"
             spacesMoved = 0
-            for b in sort_blocks(blocks):
+            copyList = blocks.copy()
+            for b in sort_blocks(copyList):
                 if i == b[1]:
                     square = "[]"
                     if (b == snake_head):
                         square = "00"
                     rowString += " " * (b[0]-spacesMoved) + square
-                    spacesMoved += b[0]+2
+                    spacesMoved = b[0]+2
             rowString += " " * (20-spacesMoved) + "|" + "\n"
         else:
             rowString += " " + "-" * 20 + "\n"
     print(rowString)
-    # print(f"MoveY: {MoveY}")
 
 def check_input():
-    global MoveX, MoveY, blocks, direction
+    global MoveX, MoveY, blocks, direction, keyPressed
     if msvcrt.kbhit():
         c = msvcrt.getwch()
         if c == 'q':
             print(f"MoveX: {MoveX}")
             print(f"MoveY: {MoveY}")
             quit()
-        elif c == 'M' or c == "d":
-            if direction % 2 == 0:
-                direction = 1
-        elif c == 'H' or c == "w":
-            if direction % 2 == 1:
-                direction = 2
-        elif c == 'K' or c == "a":
-            if direction % 2 == 0:
-                direction = 3
-        elif c == 'P' or c == "s":
-            if direction % 2 == 1:
-                direction = 4
-        # elif c != "à":
-        #     print(f"\"{c}\"")
+        if not keyPressed:
+            if c == 'M' or c == "d":
+                if direction % 2 == 0:
+                    direction = 1
+                keyPressed = True
+            elif c == 'H' or c == "w":
+                if direction % 2 == 1:
+                    direction = 2
+                keyPressed = True
+            elif c == 'K' or c == "a":
+                if direction % 2 == 0:
+                    direction = 3
+                keyPressed = True
+            elif c == 'P' or c == "s":
+                if direction % 2 == 1:
+                    direction = 4
+                keyPressed = True
 
 elapsed_time = 0
 def gameloop():
-    global blocks, MoveX, MoveY, direction, elapsed_time, snake_head, old_pos, length
+    global blocks, MoveX, MoveY, direction, elapsed_time, snake_head, old_pos, length, keyPressed
     while True:
         start_time = time.perf_counter()
         check_input()
 
         if elapsed_time >= 1/8:
+            keyPressed = False
             print_board()
             elapsed_time = 0
+
+            # copy = blocks.copy()
+            # copy.remove([MoveX, MoveY])
 
             if direction == 1:
                 MoveX += 2
@@ -91,17 +94,31 @@ def gameloop():
             if MoveY < 0:
                 MoveY = ROWS-2
 
-            blocks = []
+            # if copy.count([MoveX, MoveY]) >= 1:
+            print(blocks)
+            if blocks.count([MoveX, MoveY]) >= 1:
+                print_board()
+                print("You Died!\n")
+                quit()
+
             snake_head = [MoveX, MoveY]
-            blocks.append(snake_head)
-
             blocks.append(old_pos)
-            old_pos=(MoveX, MoveY)
-            print(len(blocks))
-            if len(blocks) > length:
-                del blocks[0]
+            old_pos=[MoveX, MoveY]
 
-        # time.sleep(1/16)
+            print(f"len: {len(blocks)}")
+            if len(blocks) > length:
+                # if blocks[0] == snake_head:
+                #     del blocks[1]
+                # else:
+                del blocks[0]
+            
+
+            snake_head = blocks[-1]
+            # if not (snake_head in blocks):
+            #     # blocks.remove(snake_head)
+            #     blocks.append(snake_head)
+            #     print("yay")
+
         end_time = time.perf_counter()
         elapsed_time += (end_time-start_time)
 
