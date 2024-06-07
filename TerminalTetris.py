b5ab4af15aa4548f4fd7009493bb7a4e3f0ed6ad
Samrocks58 <60971882+Samrocks58@users.noreply.github.com>
2024-06-07 16:32:37 -0400

    almost close to tetris

--- TerminalTetris.py
+++ TerminalTetris.py
@@ -15,14 +15,25 @@ blocks = [[MoveX, MoveY]]
 old_pos = [MoveX, MoveY]
 length = 1
 keyPressed = False
+dropped = False
 colors = ["red", "blue"]
 IPiece = [[[0,0], [1*2,0], [2*2,0], [3*2,0]], [[0,0], [0,1], [0,2], [0,3]]]
-OPiece = [[0,0], [1*2,0], [0,1], [1*2,1]]
+OPiece = [[[0,0], [1*2,0], [0,1], [1*2,1]]]
 TPiece = [[[0,0], [1*2,0], [2*2,0], [1*2,1]], [[1*2,0], [0,1], [1*2,1], [1*2,2]], [[1*2,0], [0,1], [1*2,1], [2*2,1]], [[0,0], [0,1], [0,2], [1*2,1]]]
 LPiece = [[[0,0], [1*2,0], [2*2,0], [0,1]], [[0,0], [1*2,0], [1*2,1], [1*2,2]], [[0,1], [1*2,1], [2*2,1], [2*2,0]], [[0,0], [0,1], [0,2], [1*2,2]]]
 JPiece = [[[0,0], [1*2,0], [2*2,0], [2*2,1]], [[0,2], [1*2,0], [1*2,1], [1*2,2]], [[0,1], [1*2,1], [2*2,1], [0,0]], [[0,0], [0,1], [0,2], [1*2,0]]]
 ZPiece = [[[0,0], [1*2,0], [1*2,1], [2*2,1]], [[1*2,0], [0,1], [1*2,1], [0,2]]]
 SPiece = [[[1*2,0], [0,1], [1*2,1], [2*2,0]], [[0,0], [0,1], [1*2,1], [1*2,2]]]
+pieces = [IPiece, OPiece, TPiece, LPiece, JPiece, ZPiece, SPiece]
+pieceIndex = random.randint(0, 6)
+rotationIndex = 0
+
+def make_blocks(p, r):
+    global blocks, pieces
+    offsets = pieces[p][r % len(pieces[p])]
+    blocks = []
+    for x in offsets:
+        blocks.append([MoveX+x[0], MoveY+x[1]])
 
 # This function does most of the GUI for the whole program by calculating the offset of each block
 def print_board():
@@ -50,7 +61,7 @@ def print_board():
     print(rowString)
 
 def check_input():
-    global MoveX, MoveY, blocks, passive_blocks
+    global MoveX, MoveY, blocks, passive_blocks, rotationIndex, dropped
     if msvcrt.kbhit():
         c = msvcrt.getwch()
         if c == 'q':
@@ -65,16 +76,40 @@ def check_input():
                     MoveX -= 2
                     MoveX = max(0, MoveX)
             elif c == "s" or c == "x":
-                # Maybe Instant drop later (Or make it spacebar)
-                # Or the rotate key
-                pass
+                rotationIndex += 1
+                if rotationIndex >= 4:
+                    rotationIndex = 0
+            elif c == " ":
+                print("DROP")
+                MoveY = ROWS-3
+                reachedBottom = False
+                while not reachedBottom:
+                    for p in passive_blocks:
+                        for x in blocks:
+                            if x == p:
+                                reachedBottom = True
+                                for b in blocks:
+                                    b[1] -= 1
+                    for b in blocks:
+                        b[1] += 1
+                        if b[1] >= ROWS-3:
+                            reachedBottom = True
+                dropped = True
+                # bigOffset = 1
+                # for b in blocks:
+                #     bigOffset = max(bigOffset, b[1])
+                # for p in passive_blocks:
+                #     if p[0] == MoveX and p[1] <= MoveY:
+                #         MoveY = p[1]-bigOffset
+                #     make_blocks(pieceIndex, rotationIndex)
+                # Instant DROP key
 
 elapsed_time = 0
 fall_timer = 0
 coinCollected = False
 game_over = False
 def gameloop():
-    global blocks, passive_blocks, MoveX, MoveY, elapsed_time, fall_timer, game_over
+    global blocks, passive_blocks, MoveX, MoveY, elapsed_time, fall_timer, game_over, pieceIndex, rotationIndex, dropped
     while True:
         start_time = time.perf_counter()
         check_input()
@@ -90,15 +125,22 @@ def gameloop():
                 
             print_board()
 
-            if (MoveY == ROWS-3) or ([MoveX, MoveY+1] in passive_blocks):
-                passive_blocks.append([MoveX, MoveY])
-                MoveX = startX
-                MoveY = startY
+            for b in blocks:
+                if (b[1] == ROWS-3) or ([b[0], b[1]+1] in passive_blocks) or dropped:
+                    # if (MoveY == ROWS-3) or ([MoveX, MoveY+1] in passive_blocks):
+                    # passive_blocks.append([MoveX, MoveY])
+                    passive_blocks.extend(blocks)
+                    MoveX = startX
+                    MoveY = startY
+                    dropped = False
+                    break
 
-            blocks = []
-            blocks.append([MoveX, MoveY])
+            # blocks = []
+            # blocks.append([MoveX, MoveY])
+            make_blocks(pieceIndex, rotationIndex)
         
-        if fall_timer >= 3/4:
+        # if fall_timer >= 3/4:
+        if fall_timer >= 3/8:
             fall_timer = 0
             MoveY += 1
             # if (MoveY == ROWS-3) or ([MoveX, MoveY+1] in passive_blocks):
